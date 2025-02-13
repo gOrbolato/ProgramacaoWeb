01f5984fd1e5bb5b86136f534de5b1ec1c446e7c
Guilherme Orbolato <orbolato.guilherme@gmail.com>
2025-02-13 10:01:03 -0300

    Aula 12-02

--- example-app/app/Http/Controllers/Controller.php
+++ example-app/app/Http/Controllers/Controller.php
@@ -1,8 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
-abstract class Controller
-{
-    //
+class ExerciciosController extends Controller{
+    public function abrirFormulario(){
+        return view('formulario');
+    }
+    public function calcularFormulario(Request $request){
+        $valor1 = intval($request->input('valor1'));
+        $valor2 = intval($request->input('valor2'));
+        $soma = $valor1 + $valor2;
+        return view('formulario', compact('soma'));
+    }
 }
+
+
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExerciciosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/formulario', [ExerciciosController::class, 'abrirFormulario']);
+
+Route::get('/formulario_resposta', [ExerciciosController::class, 'calcularFormulario']);
